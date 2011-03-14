6cf21a3a0ce5ead001efc5f802d17810113a5ec7
Mark Rogers <mark.rogers@medtelligent.com>
2011-03-14 17:12:06 -0500

    start playing the music

--- djpandora/management/commands/auth_rpc_server.py
+++ djpandora/management/commands/auth_rpc_server.py
@@ -1,6 +1,7 @@
 import xmlrpclib
 
 from django.core.management.base import NoArgsCommand
+from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
 
 from djpandora import models, utils
@@ -15,3 +16,15 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         s = xmlrpclib.ServerProxy('http://localhost:8123')
         s.login(settings.PANDORA_USER, settings.PANDORA_PASS)
+        try:
+            station = models.Station.objects.get(current=True)
+            s.play_station(station.pandora_id)
+        except ObjectDoesNotExist:
+            stations = models.Station.objects.all().order_by('?')
+            if not stations:
+                print "No stations"
+                return
+            station = stations[0]
+            station.current = True
+            station.save()
+            s.play_station(station.pandora_id)
