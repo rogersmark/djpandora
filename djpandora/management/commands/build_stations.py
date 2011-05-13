0d21aa7cc7d08611f1f35a30a0155196da621787
Mark Rogers <f4nt@f4ntasmic.com>
2011-05-13 16:28:07 -0500

    Closes #17

--- djpandora/management/commands/build_stations.py
+++ djpandora/management/commands/build_stations.py
@@ -1,4 +1,5 @@
 import xmlrpclib
+import logging
 
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
@@ -13,10 +14,17 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         s = utils.get_pandora_rpc_conn()
         stations = s.get_stations()
+        pandora_ids = []
         for (id, station_name) in stations.items():
             obj, created = models.Station.objects.get_or_create(
-                name=station_name, pandora_id=id
+                name=station_name, pandora_id=id, account=settings.PANDORA_USER
             )
-            if not obj.account:
-                obj.account = settings.PANDORA_USER
-                obj.save()
+            pandora_ids.append(id)
+
+        stations = models.Station.objects.filter(account=settings.PANDORA_USER)
+        for station in stations:
+            if station.pandora_id not in pandora_ids:
+                logging.info("Removed station: %s" % station)
+                station.delete()
+
+        
